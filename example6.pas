d09d46782160c368834a33cde3b6a481275d7629
Ambar Suarez <ambarsuarez@Ambars-MacBook.local>
2017-06-11 12:39:31 -0600

    Adding nuevos mensajes de error

--- example6.pas
+++ example6.pas
@@ -12,10 +12,10 @@ var
      end;
 
 begin
-   a:= f;       {tira error si se le asigna f por ejemplo, pero hay un null pointer exception}
-   b:= 15 + a;   {si 15 se cambia por e tira un null pointer exception}
+   a:= 10;       {tira error si se le asigna f por ejemplo, pero hay un null pointer exception}
+   b:= a + a;   {si 15 se cambia por e tira un null pointer exception}
    c:= 13 + 10;    {si b se cambia por e que es un integer, no tira error}
    e:= 'Ambar' + 'hola'; {si hol se cambia por solo l hay un null pointer exception}
-   f:= 'Ambar' + a; {No funciona si le asigno 'Ambar+a'}
+   f:= 'Ambar' + e; {No funciona si le asigno 'Ambar+a'}
 end
 .
