2e11ed045f29b88aa83c0f77c26af60e10cc6d4e
Ambar Suarez <ambarsuarez@Ambars-MacBook.local>
2017-06-11 12:21:26 -0600

    Lo compile y me sale que hubieron cambios, github tonto

--- example6.pas
+++ example6.pas
@@ -14,7 +14,7 @@ var
 begin
    a:= 10;       {tira error si se le asigna f por ejemplo, pero hay un null pointer exception}
    b:= 15 + a;   {si 15 se cambia por e tira un null pointer exception}
-   c:= a + e;    {si b se cambia por e que es un integer, no tira error}
+   c:= a + b;    {si b se cambia por e que es un integer, no tira error}
    e:= 'Ambar' + 'hol'; {si hol se cambia por solo l hay un null pointer exception}
    f:= 'Ambar' + a; {No funciona si le asigno 'Ambar+a'}
 end
