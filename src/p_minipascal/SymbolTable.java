5f7dc2640721dd4df1a226e2b7a24ac51b0b41b2
ambarsf <ambarsuarez@Ambars-MacBook.local>
2017-06-17 21:40:36 -0600

    Arreglado el null pointer, merci Fer

--- src/p_minipascal/SymbolTable.java
+++ src/p_minipascal/SymbolTable.java
@@ -197,7 +197,7 @@ public class SymbolTable {
         if(simbolo.getTipo() == null){
             simbolo.setTipo(UltimoTipo);
             simbolo.setAmbito(Ambito);
-            System.out.println("AGREGAR TIPO");
+            //System.out.println("AGREGAR TIPO");
             if(UltimoTipo.contains("Array")){
                 System.out.println("ENTRO A ULTIMO TIPO CONTAINS ARRAY");
                 String tipoArreglo = "";
@@ -289,7 +289,7 @@ public class SymbolTable {
             tablaSimbolos.remove(nombre);//Elimino para actualizar
             tablaSimbolos.put(nombre, simbolo);
             
-            imprimir();
+            //imprimir();
             return simbolo;
         }
         else
